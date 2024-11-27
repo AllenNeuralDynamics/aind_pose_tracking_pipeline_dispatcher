11be9a8f145430a3495bdf183b11a2c8d28d9c24
Di-Wang-AIND <di.wang@alleninstitute.org>
2024-11-27 00:21:42 +0000

    initial commit

--- code/run_capsule.py
+++ code/run_capsule.py
@@ -1,7 +1,436 @@
 """ top level run script """
 
+import json
+import logging
+import os
+import re
+import sys
+import time
+from glob import glob
+from pathlib import Path
+from typing import List, Tuple, Union
+
+from aind_codeocean_api.codeocean import CodeOceanClient
+from aind_codeocean_api.models.data_assets_requests import (
+    CreateDataAssetRequest, Source, Sources)
+from utils import utils
+
+PathLike = Union[str, Path]
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s - %(levelname)s : %(message)s",
+    datefmt="%Y-%m-%d %H:%M",
+    handlers=[
+        logging.StreamHandler(),
+        # logging.FileHandler("test.log", "a"),
+    ],
+)
+logger = logging.getLogger(__name__)
+
+PIPELINE_VERSION = "2.0.2" # TODO
+def get_data_config(
+    data_folder: PathLike,
+    data_description_path: str = "data_description.json",
+) -> Tuple:
+    """
+    Returns the first behavior dataset found
+    in the data folder
+
+    Parameters
+    -----------
+    data_folder: str
+        Path to the folder that contains the data
+
+    data_description_path: str
+        Path for the data description
+
+    Returns
+    -----------
+    Tuple[str, list]
+        Str: Empty string if the processing manifest
+        was not found
+
+        List: Empty list if no investigators in data description
+    """
+    # Returning first behavior dataset found
+    # Doing this because of Code Ocean, ideally we would have
+    # a single dataset in the pipeline
+
+    data_description_path = Path(f"{data_folder}/{data_description_path}")
+
+    if not data_description_path.exists():
+        raise ValueError(
+            f"Please, check data description path: {data_description_path}"
+        )
+
+    data_description_dict = utils.read_json_as_dict(str(data_description_path))
+
+    behavior_dataset = data_description_dict["name"]
+    investigators = data_description_dict["investigators"]
+
+    return behavior_dataset, investigators
+
+
+def create_derived_videoprocessed_metadata(
+    data_folder: PathLike, results_folder: PathLike, logger: logging.Logger
+) -> Tuple[PathLike, str]:
+    """
+    Creates the derived metadata following
+    AIND conventions.
+
+    Parameters
+    ----------
+    data_folder: PathLike
+        Path to the code ocean data folder
+
+    results_folder: PathLike
+        Path to the code ocean results folder
+
+    logger: logging.Logger
+        Logging object
+
+    Returns
+    -------
+    Tuple[PathLike, str]
+        The first position of the tuple
+        corresponds to the path where the
+        metadata was created while the
+        second position has the new name
+        of the dataset
+    """
+    logger.info("Generating derived data description")
+    raw_metadata_path = data_folder.joinpath("input_aind_metadata")
+    output_dispatch_metadata = f"{results_folder}/output_aind_metadata"
+    utils.create_folder(output_dispatch_metadata)
+
+    new_dataset_name = utils.generate_data_description(
+        raw_data_description_path=raw_metadata_path.joinpath("data_description.json"),
+        dest_data_description=output_dispatch_metadata,
+        process_name="videoprocessed",
+    )
+
+    logger.info("Copying all available raw behavior metadata")
+
+    # This is the AIND metadata
+    found_metadata = utils.copy_available_metadata(
+        input_path=raw_metadata_path,
+        output_path=output_dispatch_metadata,
+        files_to_copy=[
+            "acquisition.json",
+            "instrument.json",
+            "subject.json",
+            "procedures.json",
+            "session.json",
+        ],
+    )
+
+    logger.info(f"Copied metadata from {raw_metadata_path}: {found_metadata}")
+    logger.info(
+        f"Metadata in raw folder {raw_metadata_path}: {os.listdir(raw_metadata_path)}"
+    )
+    logger.info(
+        f"Metadata in folder {output_dispatch_metadata}: {os.listdir(output_dispatch_metadata)}"
+    )
+
+    return output_dispatch_metadata, new_dataset_name
+
+
+def copy_intermediate_data(
+    output_dispatch_metadata: PathLike,
+    pred_folders: List[PathLike],
+    eval_folders: List[PathLike],
+    new_dataset_name: str,
+    bucket_path: str,
+    results_folder: PathLike,
+    logger: logging.Logger,
+) -> str:
+    """
+    Copies the prediction and evaluation metadata
+    to the destination bucket to make it available
+    to scientists as soon as possible.
+
+    Parameters
+    ----------
+    output_dispatch_metadata: PathLike
+        Path where the new metadata (derived)
+        for the processed dataset is located
+
+    pred_folders: List[PathLike]
+        Video prediction folders generated in the
+        prediction step.
+
+    eval_folders: List[PathLike]
+        Evaluation folders generated in the
+        evaluation step.
+
+    new_dataset_name: str
+        New dataset name where the data will
+        be copied following the aind conventions
+        e.g., s3://{bucket_path}/{new_dataset_name}
+
+    bucket_path: str
+        S3 path where the data will be moved.
+        Do not include 's3://' since this is
+        automatically added.
+
+    results_folder: PathLike
+        Results folder path in Code Ocean
+
+    logger: logging.Logger
+        Logging object
+
+    Returns
+    -------
+    Tuple[str, str]
+        The first position is the path where the dataset
+        was moved. e.g., s3://{bucket_path}/{new_dataset_name}
+        It includes the "s3://" prefix. 
+        e.g., s3://{bucket_path}/{new_dataset_name}/{output_prediction}
+    """
+
+    pred_processings = []
+    eval_processings = []
+
+    for pred_folder in pred_folders:
+        processing_jsons = [
+            p
+            for p in glob(f"{pred_folder}/*processing*.json")
+            if "manifest" not in str(p)
+        ]
+        pred_processings.append(processing_jsons)
+
+    for eval_folder in eval_folders:
+        processing_jsons = [
+            p
+            for p in glob(f"{eval_folder}/*processing*.json")
+            if "manifest" not in str(p)
+        ]
+        eval_processings.append(processing_jsons)
+
+    # Flattening list
+    processing_paths = list()
+    combined_processing_list = pred_processings + eval_processings
+    for sub_list in combined_processing_list:
+        processing_paths += sub_list
+    logger.info(f"Processing paths: {processing_paths}")
+
+    try:
+        output_filename = utils.compile_processing_jsons(
+            processing_paths=processing_paths,
+            output_general_processing=output_dispatch_metadata,
+            processor_full_name="Di Wang",
+            pipeline_version=PIPELINE_VERSION,
+        )
+
+    except Exception as e:
+        print(f"Error while compiling processing manifests: {e}")
+        output_filename = None
+
+    logger.info(f"Compiled processing.json in path {output_filename}")
+
+    s3_path = f"s3://{bucket_path}/{new_dataset_name}"
+    logger.info(f"Copy files to path {s3_path}")
+
+
+    # Copying derived metadata
+    output_dispatch_metadata = Path(output_dispatch_metadata)
+    print(f"output_dispatch_metadata: {output_dispatch_metadata}")
+    # for out in utils.execute_command_helper(
+    #     f"aws s3 cp --recursive {output_dispatch_metadata} {s3_path}"
+    # ):
+    #     logger.info(out)
+
+
+    # # Copying prediction data
+    # output_prediction = "pred_outputs" # TODO: evaluation folder name
+    # dest_pred_path = f"{s3_path}/{output_prediction}"
+
+    # for pred_folder in pred_folders:
+    #     logger.info(f"Copying data from {pred_folder} to {dest_pred_path}")
+    #     pred_folder = Path(pred_folder)
+
+    #     if pred_folder.exists():
+    #         for out in utils.execute_command_helper(
+    #             f"aws s3 cp --recursive {pred_folder} {dest_pred_path}"
+    #         ):
+    #             logger.info(out)
+    #     else:
+    #         raise ValueError(f"Folder {pred_folder} does not exist!")
+
+
+    # # Copying evaluation data
+    # output_evaluation = "eval_outputs" # TODO: evaluation folder name
+    # dest_eval_path = f"{s3_path}/{output_evaluation}"
+
+    # for eval_folder in eval_folders:
+    #     logger.info(f"Copying data from {eval_folder} to {dest_eval_path}")
+    #     eval_folder = Path(eval_folder)
+
+    #     if eval_folder.exists():
+    #         for out in utils.execute_command_helper(
+    #             f"aws s3 cp --recursive {eval_folder} {dest_eval_path}"
+    #         ):
+    #             logger.info(out)
+    #     else:
+    #         raise ValueError(f"Folder {eval_folder} does not exist!")
+
+    # utils.save_string_to_txt(
+    #     f"Video prediction and evaluation dataset saved in: {s3_path}",
+    #     f"{results_folder}/output_video_pred_evaluation.txt",
+    # )
+
+    return s3_path
+
+
+def dispatch(s3_path: str, results_folder: PathLike, bucket: str):
+    """
+    Register s3 bucket
+
+    Parameters
+    ----------
+    s3_path: str
+        Path to the s3 path
+
+    results_folder: str
+        Path pointing to the results folder
+
+    bucket: str
+        Bucket name where the data is stored
+    """
+
+    logger.info(f"Provided s3_path: {s3_path}")
+
+    codeocean_domain = os.getenv("API_KEY")
+    co_token = os.getenv("API_SECRET")
+
+    print(f"codeocean_domain: {codeocean_domain}")
+    print(f"co_token: {co_token}")
+    co_client = CodeOceanClient(domain=codeocean_domain, token=co_token)
+
+    print(f"co_client: {co_client}")
+
+
+    # Getting path in S3
+    dataset_to_predict = s3_path
+
+    for modality in ["ecephys", "behavior"]:
+        pattern = (
+            modality + r"_\d+_\d{4}-\d{2}-\d{2}_\d{2}-\d{2}-\d{2}"
+            r"_videoprocessed_\d{4}-\d{2}-\d{2}_\d{2}-\d{2}-\d{2}"
+        )
+        print(f"pattern: {pattern}")
+        found_pattern = re.findall(pattern=pattern, string=dataset_to_predict)
+        print(f"found_pattern: {found_pattern}")
+
+        # Extract the data asset info
+        if len(found_pattern):
+            dataset_to_predict = found_pattern[0]
+            print(f"dataset_to_predict: {dataset_to_predict}")
+
+            video_pred_eval_tags = [modality, "behavior-videos", "processed", "derived"] # TODO: add subject_id?
+            print(f"video_pred_eval_tags: {video_pred_eval_tags}")
+
+
+            # # Registering AWS data asset
+            # aws_source = Sources.AWS(
+            #     bucket=bucket,
+            #     prefix=dataset_to_predict,
+            #     keep_on_external_storage=True,
+            #     public=True,
+            # )
+            # source = Source(aws=aws_source)
+
+            # create_data_asset_request = CreateDataAssetRequest(
+            #     name=dataset_to_predict,
+            #     tags=video_pred_eval_tags,
+            #     mount=dataset_to_predict,
+            #     source=source,
+            #     custom_metadata=None,
+            # )
+
+            # input_json_data = json.loads(create_data_asset_request.json_string)
+
+            # # Register the behavior video processed dataset
+            # try:
+            #     data_asset_reg_response = co_client.create_data_asset(
+            #         request=input_json_data
+            #     )
+
+            #     response_contents = data_asset_reg_response.json()
+            #     logger.info(f"Created data asset in Code Ocean: {response_contents}")
+
+            #     # Making the created data asset available for everyone
+            #     make_data_viewable(co_client, response_contents)
+
+            # except Exception as e:
+            #     logger.warning(f"Error registering data asset in the API call. Error: {e}")
+
+        else:
+            logger.warning("Error registering data asset")
+
+
+
 def run():
     """ basic run function """
-    pass
+    # Absolute paths of common Code Ocean folders
+    data_folder = Path(os.path.abspath("../data"))
+    results_folder = Path(os.path.abspath("../results"))
+
+    # It is assumed that these files
+    # will be in the data folder
+    required_input_elements = [
+        f"{data_folder}/input_aind_metadata/data_description.json",
+    ]
+
+    missing_files = utils.validate_capsule_inputs(required_input_elements)
+
+    if len(missing_files):
+        raise ValueError(
+            f"We miss the following files in the capsule input: {missing_files}"
+        )
+
+    dataset_name, investigators = get_data_config(
+            data_folder=data_folder,
+            data_description_path="input_aind_metadata/data_description.json",
+        )
+    print(f"dataset_name: {dataset_name}")
+    print(f"investigators: {investigators}")
+
+    # Creating new metadata for videoprocessed dataset
+    output_dispatch_metadata, new_dataset_name = create_derived_videoprocessed_metadata(
+        data_folder=data_folder, results_folder=results_folder, logger=logger
+    )
+
+    # get prediction and evaluation outputs
+    pred_folders = glob(f"{data_folder}/pred_outputs")
+    eval_folders = glob(f"{data_folder}/eval_outputs/eval_outputs") # TODO
+
+    bucket_path = "aind-open-data"
+
+    print(f"output_dispatch_metadata: {output_dispatch_metadata}")
+    print(f"pred_folders: {pred_folders}")
+    print(f"eval_folders: {eval_folders}")
+    print(f"new_dataset_name: {new_dataset_name}")
+    print(f"bucket_path: {bucket_path}")
+    print(f"results_folder: {results_folder}")
+
+    s3_path = copy_intermediate_data(
+        output_dispatch_metadata=output_dispatch_metadata,
+        pred_folders=pred_folders,
+        eval_folders=eval_folders,
+        new_dataset_name=new_dataset_name,
+        bucket_path=bucket_path,
+        results_folder=results_folder,
+        logger=logger,
+    )
+
+    print(f"s3_path: {s3_path}")
+
+    dispatch(
+        s3_path=s3_path,
+        results_folder=results_folder,
+        bucket=bucket_path,
+    )
 
 if __name__ == "__main__": run()
---
+++ code/utils/utils.py
@@ -0,0 +1,341 @@
+import os
+import json
+from datetime import datetime
+import shutil
+import subprocess
+from pathlib import Path
+from typing import Any, List, Optional, Union
+
+from aind_data_schema.base import AindCoreModel
+from aind_data_schema.core.data_description import (DerivedDataDescription,
+                                                    Funding)
+from aind_data_schema.core.processing import (DataProcess, PipelineProcess,
+                                              Processing)
+from aind_data_schema_models.modalities import Modality
+from aind_data_schema_models.organizations import Organization
+from aind_data_schema_models.pid_names import PIDName
+from aind_data_schema_models.platforms import Platform
+from pydantic import TypeAdapter
+PathLike = Union[str, Path]
+
+
+def compile_processing_jsons(
+    processing_paths: List[str],
+    output_general_processing: str,
+    processor_full_name: str,
+    pipeline_version: str,
+) -> str:
+    """
+    processing_paths: List[str]
+        Paths where the processing jsons are located.
+        The order of the list determines which data
+        process goes first into the final processing.json
+
+    output_general_processing: str
+        Path where we will output the general processing
+        json
+
+    processor_full_name: str
+        Person in charged to run the pipeline
+
+    pipeline_version: str
+        Pipeline version
+
+    Returns
+    -------
+    str:
+        Path where the processing json was saved
+    """
+    data_processes = []
+    for processing_path in processing_paths:
+        curr_processing = read_json_as_dict(str(processing_path))
+        processing_adapter = TypeAdapter(Processing)
+        curr_processing_obj = processing_adapter.validate_python(curr_processing)
+
+        for data_process in curr_processing_obj.processing_pipeline.data_processes:
+            data_processes.append(data_process)
+
+    output_filename = generate_processing(
+        data_processes=data_processes,
+        dest_processing=str(output_general_processing),
+        processor_full_name=processor_full_name,
+        pipeline_version=pipeline_version,
+    )
+
+    return output_filename
+
+
+def copy_file(input_filename: PathLike, output_filename: PathLike):
+    """
+    Copies a file to an output path
+
+    Parameters
+    ----------
+    input_filename: PathLike
+        Path where the file is located
+
+    output_filename: PathLike
+        Path where the file will be copied
+    """
+
+    try:
+        shutil.copy(input_filename, output_filename)
+
+    except shutil.SameFileError:
+        raise shutil.SameFileError(
+            f"The filename {input_filename} already exists in the output path."
+        )
+
+    except PermissionError:
+        raise PermissionError(
+            "Not able to copy the file. Please, check the permissions in the output path."
+        )
+
+def copy_available_metadata(
+    input_path: PathLike, output_path: PathLike, files_to_copy: List[str]
+) -> List[PathLike]:
+    """
+    Copies all the valid metadata from the aind-data-schema
+    repository that exists in a given path.
+
+    Parameters
+    -----------
+    input_path: PathLike
+        Path where the metadata is located
+
+    output_path: PathLike
+        Path where we will copy the found
+        metadata
+
+    files_to_copy: List[str]
+        List with the filenames of the metadata
+        that we need to copy to the fused asset
+
+    Returns
+    --------
+    List[PathLike]
+        List with the metadata files that
+        were copied
+    """
+
+    print("Files to copy: ", files_to_copy)
+    # Making sure the paths are pathlib objects
+    input_path = Path(input_path)
+    output_path = Path(output_path)
+
+    found_metadata = []
+
+    for metadata_filename in files_to_copy:
+        metadata_filename = input_path.joinpath(metadata_filename)
+
+        if metadata_filename.exists():
+            found_metadata.append(metadata_filename)
+
+            # Copying file to output path
+            output_filename = output_path.joinpath(metadata_filename.name)
+            copy_file(metadata_filename, output_filename)
+
+    return found_metadata
+
+
+def create_folder(dest_dir: PathLike, verbose: Optional[bool] = False) -> None:
+    """
+    Create new folders.
+
+    Parameters
+    ------------------------
+
+    dest_dir: PathLike
+        Path where the folder will be created if it does not exist.
+
+    verbose: Optional[bool]
+        If we want to show information about the folder status. Default False.
+
+    Raises
+    ------------------------
+
+    OSError:
+        if the folder exists.
+
+    """
+
+    if not (os.path.exists(dest_dir)):
+        try:
+            if verbose:
+                print(f"Creating new directory: {dest_dir}")
+            os.makedirs(dest_dir)
+        except OSError as e:
+            if e.errno != os.errno.EEXIST:
+                raise
+
+
+def generate_data_description(
+    raw_data_description_path,
+    dest_data_description,
+    process_name: Optional[str] = "videoprocessed",
+):
+    """
+    Generates data description for the output folder.
+
+    Parameters
+    -------------
+
+    raw_data_description_path: PathLike
+        Path where the data description file is located.
+
+    dest_data_description: PathLike
+        Path where the new data description will be placed.
+
+    process_name: str
+        Process name of the new dataset
+
+
+    Returns
+    -------------
+    str
+        New folder name for the fused
+        data
+    """
+
+    f = open(raw_data_description_path, "r")
+    data = json.load(f)
+
+    if isinstance(data["institution"], dict) and "abbreviation" in data["institution"]:
+        institution = data["institution"]["abbreviation"]
+
+    investigators = data["investigators"]
+    platform = data["platform"]
+    modality = data["modality"]
+
+    if len(investigators) and len(investigators[0]):
+        investigators = [PIDName.parse_obj(inv) for inv in investigators]
+
+    else:
+        investigators = [PIDName(name="Unknown")]
+
+    # from_data_description
+    funding_adapter = TypeAdapter(Funding)
+    funding_sources = [
+        funding_adapter.validate_python(fund) for fund in data["funding_source"]
+    ]
+    # Ensuring backwards compatibility
+    derived = DerivedDataDescription(
+        creation_time=datetime.now(),
+        input_data_name=data["name"],
+        process_name=process_name,
+        institution=Organization.from_abbreviation(institution),
+        funding_source=funding_sources,
+        group=data["group"],
+        investigators=investigators,
+        platform=platform,
+        project_name=data["project_name"],
+        restrictions=data["restrictions"],
+        modality=modality,
+        subject_id=data["subject_id"],
+    )
+
+    # derived.write_standard_file(output_directory=dest_data_description)
+    with open(f"{dest_data_description}/data_description.json", "w") as f:
+        f.write(derived.model_dump_json())
+
+    return derived.name
+
+
+def read_json_as_dict(filepath: str) -> dict:
+    """
+    Reads a json as dictionary.
+
+    Parameters
+    ------------------------
+
+    filepath: PathLike
+        Path where the json is located.
+
+    Returns
+    ------------------------
+
+    dict:
+        Dictionary with the data the json has.
+
+    """
+
+    dictionary = {}
+
+    if os.path.exists(filepath):
+        with open(filepath) as json_file:
+            dictionary = json.load(json_file)
+
+    return dictionary
+
+def validate_capsule_inputs(input_elements: List[str]) -> List[str]:
+    """
+    Validates input elemts for a capsule in
+    Code Ocean.
+
+    Parameters
+    -----------
+    input_elements: List[str]
+        Input elements for the capsule. This
+        could be sets of files or folders.
+
+    Returns
+    -----------
+    List[str]
+        List of missing files
+    """
+
+    missing_inputs = []
+    for required_input_element in input_elements:
+        required_input_element = Path(required_input_element)
+
+        if not required_input_element.exists():
+            missing_inputs.append(str(required_input_element))
+
+    return missing_inputs
+
+def generate_processing(
+    data_processes: List[DataProcess],
+    dest_processing: str,
+    processor_full_name: str,
+    pipeline_version: str,
+) -> str:
+    """
+    Generates data description for the output folder.
+
+    Parameters
+    ------------------------
+
+    data_processes: List[dict]
+        List with the processes aplied in the pipeline.
+
+    dest_processing: PathLike
+        Path where the processing file will be placed.
+
+    processor_full_name: str
+        Person in charged of running the pipeline
+        for this data asset
+
+    pipeline_version: str
+        Terastitcher pipeline version
+
+    Returns
+    -------
+    str
+        Path where the processing json was saved
+    """
+    # flake8: noqa: E501
+    processing_pipeline = PipelineProcess(
+        data_processes=data_processes,
+        processor_full_name=processor_full_name,
+        pipeline_version=pipeline_version,
+        pipeline_url="https://codeocean.allenneuraldynamics.org/capsule/2244321/tree", # TODO
+    )
+
+    processing = Processing(
+        processing_pipeline=processing_pipeline,
+        notes="This processing was generated by compiling independent processing jsons for every step",
+    )
+
+    processing.write_standard_file(output_directory=dest_processing)
+
+    return dest_processing
